158fbd6caecd4f8e1286a455d2363522050e23ce
Queno <queno@Quenos-iMac.local>
2019-07-24 13:33:58 +0500

    new

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  options: InAppBrowserOptions = {
+    location: 'no',//Or 'no'
+    hidden: 'no', //Or  'yes'
+    clearcache: 'yes',
+    clearsessioncache: 'yes',
+    footer: 'yes',
+    footercolor: '#25a9b6',
+    zoom: 'yes',//Android only ,shows browser zoom controls
+    hardwareback: 'no',
+    mediaPlaybackRequiresUserAction: 'no',
+    shouldPauseOnSuspend: 'no', //Android only
+    closebuttoncaption: 'Close', //iOS only
+    disallowoverscroll: 'no', //iOS only
+    toolbar: 'yes', //iOS only
+    enableViewportScale: 'no', //iOS only
+    allowInlineMediaPlayback: 'no',//iOS only
+    presentationstyle: 'pagesheet',//iOS only
+    fullscreen: 'yes',//Windows only
+  };
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private iab: InAppBrowser) {
 
   }
 
+openQueno() {
+    let target = "_blank";
+    this.iab.create("http://www.thequeno.com/", target, this.options);
+  }
 }
